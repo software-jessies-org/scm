038151d58d26bf809d7c076b232c5d3f7035587a
elliotth <elliotth@5bded901-cfd8-0310-8db1-92abb6e08623>
2004-07-19 19:57:55 +0000

    src/e/scm/CheckInWindow.java: factor out BlockingWorker...
    src/e/scm/BlockingWorker.java: ...into its own file. We should be using this
    in RevisionWindow too.
    
    
    git-svn-id: file:///home/martind/software.jessies.org/svnroot/scm@188 5bded901-cfd8-0310-8db1-92abb6e08623

---
+++ trunk/src/e/scm/BlockingWorker.java
@@ -0,0 +1,58 @@
+package e.scm;
+
+import java.awt.*;
+import javax.swing.*;
+
+public abstract class BlockingWorker extends Thread {
+    private Component component;
+    private String message;
+    private Exception caughtException;
+    
+    public BlockingWorker(Component component, String message) {
+        this.component = component;
+        this.message = message;
+        start();
+    }
+    
+    public final void run() {
+        try {
+            WaitCursor.start(component, message);
+            work();
+        } catch (Exception ex) {
+            caughtException = ex;
+        } finally {
+            WaitCursor.stop(component);
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (caughtException != null) {
+                    reportException(caughtException);
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+    
+    /**
+     * Override this to do the potentially time-consuming work.
+     */
+    public abstract void work();
+    
+    /**
+     * Override this to do the finishing up that needs to be done back
+     * on the event dispatch thread. This will only be invoked if your
+     * 'work' method didn't throw an exception (if an exception was
+     * thrown, 'reportException' will be invoked instead).
+     */
+    public abstract void finish();
+    
+    /**
+     * Invoked if 'work' threw an exception. The default implementation
+     * simply prints the stack trace. This method is run on the event
+     * dispatch thread, so you can safely modify the UI here.
+     */
+    public void reportException(Exception ex) {
+        ex.printStackTrace();
+    }
+}
