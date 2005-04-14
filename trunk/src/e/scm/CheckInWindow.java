cd13e1b4bd176478ec7498de7ea9d0156d715edd
enh <enh@5bded901-cfd8-0310-8db1-92abb6e08623>
2005-04-14 16:53:44 +0000

    src/e/scm/CheckInWindow.java: we used to have word-wrap turned on for the check-in comments, and PTextArea supports word-wrap now, so back it goes. (I'm surprised I didn't just comment it out and write FIXME next to it.)
    git-svn-id: file:///home/martind/software.jessies.org/svnroot/scm@275 5bded901-cfd8-0310-8db1-92abb6e08623

--- trunk/src/e/scm/CheckInWindow.java
+++ trunk/src/e/scm/CheckInWindow.java
@@ -337,6 +337,7 @@ public class CheckInWindow extends JFrame {
     private void initCheckInCommentArea() {
         checkInCommentArea = makeTextArea(8);
         checkInCommentArea.setEnabled(false);
+        checkInCommentArea.setWrapStyleWord(true);
     }
     
     private void readSavedComment() {
