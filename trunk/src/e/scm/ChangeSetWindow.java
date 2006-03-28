5d969c3960f0ca2a1664e7bc55907d59071b5cc3
mad <mad@5bded901-cfd8-0310-8db1-92abb6e08623>
2006-03-28 23:51:55 +0000

    src/e/scm/ChangeSetWindow.java: Create a RevisionListModel for each file in the change set, so we get the right revision numbers to pass to bk diffs.  Only enable the Show Patch button when we've got all the RevisionListModels.  This makes Subversion behavior slower linearly in the number of files in the change set for no functional benefit but fixes the BitKeeper behavior.
    BackEndWorker is a copy of the nested class from RevisionWindow with the status-setting and progress indication removed.  showToolError is a direct copy from RevisionView.  Its caller - readListOfRevisions - isn't quite such a direct copy.  I've started to separate the functionality that wants to be shared between ChangeSetWindow and RevisionWindow from that which wants to be ChangeSetWindow-specific.  I haven't got very far because I'm not sure it's worth investing much effort before finding out what's going to happen to BackEndWorker.
    
    
    git-svn-id: file:///home/martind/software.jessies.org/svnroot/scm@408 5bded901-cfd8-0310-8db1-92abb6e08623

--- trunk/src/e/scm/ChangeSetWindow.java
+++ trunk/src/e/scm/ChangeSetWindow.java
@@ -13,14 +13,16 @@ import org.jdesktop.swingworker.SwingWorker;
 
 public class ChangeSetWindow extends JFrame {
     private RevisionControlSystem backEnd;
-    private RevisionListModel initialFileRevisions;
+    private Map<String, RevisionListModel> filePathToRevisionsMap;
     
     private JList fileList;
     private PatchView patchView;
+    private JButton showPatchButton;
     
     public ChangeSetWindow(final RevisionControlSystem backEnd, final String initialFilePath, final RevisionListModel initialFileRevisions, final Revision initialFileRevision) {
         this.backEnd = backEnd;
-        this.initialFileRevisions = initialFileRevisions;
+        this.filePathToRevisionsMap = new HashMap<String, RevisionListModel>();
+        filePathToRevisionsMap.put(initialFilePath, initialFileRevisions);
         
         // FIXME: Specifying a revision that, with BitKeeper, is file-specific without specifying the file doesn't make sense.
         String title = FileUtilities.getUserFriendlyName(backEnd.getRoot().toString()) + " revision " + initialFileRevision.number;
@@ -57,7 +59,8 @@ public class ChangeSetWindow extends JFrame {
     }
     
     private JComponent makeButtonPanel() {
-        JButton showPatchButton = new JButton("Show Patch");
+        this.showPatchButton = new JButton("Show Patch");
+        showPatchButton.setEnabled(false);
         showPatchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 showPatch();
@@ -71,6 +74,120 @@ public class ChangeSetWindow extends JFrame {
         return buttonPanel;
     }
     
+    // FIXME: This is a verbatim copy of code from RevisionView.
+    private void showToolError(JList list, ArrayList<String> lines) {
+        list.setCellRenderer(new ToolErrorRenderer());
+        list.setModel(new ToolErrorListModel(lines));
+    }
+    
+    // FIXME: This is a verbatim copy of code from RevisionView.
+    private void showToolError(JList list, ArrayList<String> lines, String[] command, int status) {
+        if (status != 0) {
+            lines.add("[Command '" + StringUtilities.join(command, " ") + "' returned status " + status + ".]");
+        }
+        showToolError(list, lines);
+    }
+    
+    /**
+     * Similar to BlockingWorker. FIXME: replace both classes with SwingWorker for Java 6.
+     */
+    public abstract class BackEndWorker implements Runnable {
+        private String message;
+        private Exception caughtException;
+        
+        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> errors = new ArrayList<String>();
+        String[] command;
+        int status = 0;
+        
+        private int thisWorkerModCount;
+        
+        public BackEndWorker(final String message) {
+            this.message = message;
+        }
+        
+        public final void run() {
+            try {
+                work();
+            } catch (Exception ex) {
+                caughtException = ex;
+            }
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    if (caughtException != null) {
+                        reportException(caughtException);
+                    } else {
+                        finish();
+                    }
+                }
+            });
+        }
+        
+        /**
+         * Override this to do the potentially time-consuming work.
+         */
+        public abstract void work();
+        
+        /**
+         * Override this to do the finishing up that needs to be done back
+         * on the event dispatch thread. This will only be invoked if your
+         * 'work' method didn't throw an exception (if an exception was
+         * thrown, 'reportException' will be invoked instead).
+         */
+        public abstract void finish();
+        
+        /**
+         * Invoked if 'work' threw an exception. The default implementation
+         * simply prints the stack trace. This method is run on the event
+         * dispatch thread, so you can safely modify the UI here.
+         */
+        public void reportException(Exception ex) {
+            SimpleDialog.showDetails(ChangeSetWindow.this, message, ex);
+        }
+    }
+    
+    void reassessShowPatchAvailability() {
+        showPatchButton.setEnabled(filePathToRevisionsMap.size() == fileList.getModel().getSize());
+    }
+    
+    private void readListOfRevisions(final String filePath) {
+        if (filePathToRevisionsMap.containsKey(filePath)) {
+            return;
+        }
+        new Thread(new BackEndWorker("Getting list of revisions...") {
+            public void work() {
+                command = backEnd.getLogCommand(filePath);
+                status = ProcessUtilities.backQuote(backEnd.getRoot(), command, lines, errors);
+            }
+            
+            private RevisionListModel parseRevisions() {
+                RevisionListModel revisions = backEnd.parseLog(lines);
+                if (backEnd.isLocallyModified(filePath)) {
+                    revisions.addLocalRevision(Revision.LOCAL_REVISION);
+                }
+                return revisions;
+            }
+            
+            public void finish() {
+                if (status != 0 || errors.size() > 0) {
+                    showToolError(fileList, errors, command, status);
+                    return;
+                }
+                
+                EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        reportFileRevisions(filePath, parseRevisions());
+                    }
+                });
+            }
+            
+            public void reportFileRevisions(String filePath, RevisionListModel fileRevisions) {
+                filePathToRevisionsMap.put(filePath, fileRevisions);
+                reassessShowPatchAvailability();
+            }
+        }).start();
+    }
+    
     private void showPatch() {
         StringBuilder patch = new StringBuilder();
         patch.append("# " + getTitle() + "\n");
@@ -78,16 +195,18 @@ public class ChangeSetWindow extends JFrame {
         for (int i = 0; i < model.getSize(); ++i) {
             ChangeSetItem changeSetItem = (ChangeSetItem) model.getElementAt(i);
             
+            RevisionListModel revisions = filePathToRevisionsMap.get(changeSetItem.filename);
+            // The Subversion back-end gives us old revision numbers which aren't necessarily in the file's history.
+            Revision oldRevision = new Revision(changeSetItem.oldRevision, null, null, null, null);            
+            Revision newRevision = revisions.fromNumber(changeSetItem.newRevision);
+            
             // FIXME: don't repeat the comments for systems where every file in a revision has the same comment.
-            // FIXME: we only have the RevisionListModel for the initial file.
             // FIXME: we should display the comments for all of the revisions between oldRevision and newRevision.
-            for (String commentLine : initialFileRevisions.fromNumber(changeSetItem.newRevision).comment.split("\n")) {
+            for (String commentLine : newRevision.comment.split("\n")) {
                 patch.append("# " + commentLine + "\n");
             }
             
-            Revision oldRevision = new Revision(changeSetItem.oldRevision, null, null, null, null);
-            Revision newRevision = new Revision(changeSetItem.newRevision, null, null, null, null);
-            
+            // FIXME: we shouldn't do this on the EDT.
             ArrayList<String> lines = patchView.getPatchLines(backEnd, oldRevision, newRevision, changeSetItem.filename);
             for (String line : lines) {
                 patch.append(line + "\n");
@@ -155,8 +274,10 @@ public class ChangeSetWindow extends JFrame {
             for (ChangeSetItem changeSetItem : changeSet) {
                 model.addElement(changeSetItem);
                 fileList.setEnabled(true);
+                readListOfRevisions(changeSetItem.filename);
             }
             
+            reassessShowPatchAvailability();
             // Guess the user's most likely initial action.
             fileList.setSelectedIndex(0);
             fileList.requestFocus();
